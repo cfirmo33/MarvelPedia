18ab33100ca11c4ef1d83ccedc562665d0be8a6f
Lucia Payo <lucia.payo@n26.com>
2017-06-10 18:25:44 +0200

    First commit

---
+++ app/src/main/java/com/lucilu/marvelpedia/CharactersActivity.java
@@ -0,0 +1,13 @@
+package com.lucilu.marvelpedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharactersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_characters);
+    }
+}
