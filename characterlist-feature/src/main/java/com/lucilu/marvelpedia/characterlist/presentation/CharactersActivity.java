6370fee6d90e8c741a6758d87d4ffe181890a7ab
Lucia Payo <lucia.payo@futurice.com>
2017-07-02 13:00:06 +0200

    Create feature module with right package name

---
+++ characterlist-feature/src/main/java/com/lucilu/marvelpedia/characterlist/presentation/CharactersActivity.java
@@ -0,0 +1,15 @@
+package com.lucilu.marvelpedia.characterlist.presentation;
+
+import com.lucilu.marvelpedia.characterlist.R;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharactersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_characters);
+    }
+}
---
+++ marvelpedia-base/src/main/java/com/lucilu/marvelpedia/base/injection/components/ApplicationComponent.java
@@ -0,0 +1,23 @@
+package com.lucilu.marvelpedia.base.injection.components;
+
+import com.lucilu.marvelpedia.base.MarvelPediaApplication;
+import com.lucilu.marvelpedia.injection.modules.LoggingModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ Created by luciapayo on 11/06/2017
+ */
+@Singleton
+@Component(modules = {LoggingModule.class})
+public interface ApplicationComponent {
+
+    void inject(MarvelPediaApplication app);
+
+    @Component.Builder
+    interface Builder {
+        ApplicationComponent build();
+    }
+}
