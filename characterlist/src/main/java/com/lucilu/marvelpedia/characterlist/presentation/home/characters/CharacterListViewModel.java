21c3a1b0d8ac77c5ad21cb8dbd495725c5c79139
Lucia Payo <lucilu.p.m@gmail.com>
2017-07-16 14:59:03 +0200

    Create LiveData for character

--- characterlist/src/main/java/com/lucilu/marvelpedia/characterlist/presentation/home/characters/CharacterListViewModel.java
+++ characterlist/src/main/java/com/lucilu/marvelpedia/characterlist/presentation/home/characters/CharacterListViewModel.java
@@ -2,14 +2,24 @@ package com.lucilu.marvelpedia.characterlist.presentation.home.characters;
 
 import android.arch.lifecycle.ViewModel;
 
+import javax.inject.Inject;
+
 import io.reactivex.Observable;
+import io.reactivex.subjects.BehaviorSubject;
 
 /**
  * Created by Lucia on 10/07/2017.
  */
 class CharacterListViewModel extends ViewModel {
 
+    private BehaviorSubject<CharacterViewEntity> characterStream = BehaviorSubject.create();
+
+    @Inject
+    CharacterListViewModel() {
+        characterStream.onNext(CharacterViewEntity.builder().name("DEAD POOL").build());
+    }
+
     Observable<CharacterViewEntity> characterViewEntityObservable() {
-        return Observable.just(CharacterViewEntity.builder().name("DEAD POOL").build());
+        return characterStream;
     }
 }
